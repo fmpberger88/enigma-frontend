2a5e9e6ce55c625673af6edac9668e293a8385c5
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-22 07:18:06 +0200

    implement axios instance login register

---
+++ src/api.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import error from "eslint-plugin-react/lib/util/error.js";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const axiosInstance = axios.create({
+    baseURL: API_URL,
+})
+
+const handleError = (error) => {
+    console.log(error)
+    if (error.response) {
+        console.error(`Error: ${error.response.status} - ${error.response.data.message}`);
+        // Verwenden Sie die vom Backend bereitgestellte Fehlermeldung
+        throw new Error(error.response.data.message || error.response.data.error.message || 'An error occurred');
+    } else if (error.request) {
+        console.error('Error: No response received from server');
+        throw new Error('No response received from server');
+    } else {
+        console.error('Error:', error.message);
+        throw new Error(error.message);
+    }
+};
+
+export const register = async (userData) => {
+    try {
+        const response = await axiosInstance.post('/register', userData);
+        return response.data
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const login = async (userData) => {
+    try {
+        const response = await axiosInstance.post('/login', userData);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+}
