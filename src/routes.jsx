e268e04763d7d2c36246b7672ab2ea57de90e0f1
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-23 15:19:54 +0200

    include basic routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
+import Login from "./components/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
 import Root from "./components/Root/Root.jsx";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root />,
+        element: <Login />,
+    },
+    {
+        path: '/register',
+        element: <Register />,
+    },
+    {
+        path: '/dashboard',
+        element: <Root />
     }
 ])
 
