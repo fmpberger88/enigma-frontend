c1a280c526b1de84a52f5ba5fe10e186ce6c5ccf
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-31 09:31:20 +0200

    remove ul and use div set chat to empty list

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,20 +1,26 @@
 import NotificationIcon from "../NotificationIcon/NotificationIcon.jsx";
 import styles from "./ChatList.module.css"; // Importiere das CSS-Modul
 
-const ChatList = ({ chats, selectedChat, onSelectChat, newMessageNotifications }) => {
+const ChatList = ({ chats = [], selectedChat, onSelectChat, newMessageNotifications = {} }) => {
     return (
-        <ul className={styles.chatList}>
-            {chats.map((chat) => (
-                <li
-                    key={chat.id}
-                    className={chat.id === selectedChat?.id ? styles.selected : ''}
-                    onClick={() => onSelectChat(chat.id)}
-                >
-                    {chat.users.map((user) => user.username).join(', ')}
-                    {newMessageNotifications[chat.id] && <NotificationIcon />}
-                </li>
-            ))}
-        </ul>
+        <div className={styles.chatList}>
+            {chats.map((chat) => {
+                const userNames = chat.users && chat.users.length > 0
+                    ? chat.users.map((user) => user.username).join(', ')
+                    : 'No users available';
+
+                return (
+                    <div
+                        key={chat.id}
+                        className={`${styles.chatItem} ${chat.id === selectedChat ? styles.selected : ''}`}
+                        onClick={() => onSelectChat(chat.id)}
+                    >
+                        {userNames}
+                        {newMessageNotifications[chat.id] && <NotificationIcon />}
+                    </div>
+                );
+            })}
+        </div>
     );
 };
 
